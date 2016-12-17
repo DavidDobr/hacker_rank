112ab2d30a473f99612c01b03d0ec6573682a6bb
David Dobrinskiy <david@dobrinskiy.com>
2016-12-17 13:44:16 +0300

    delete old py directory

--- py/combinatorics.py
+++
@@ -1,9 +0,0 @@
-# S, k = input().split(' ')
-S, k = 'HACK 2'.split(' ')
-
-S = sorted(S)
-k = int(k)
-
-from itertools import combinations_with_replacement
-
-print(*list(map(''.join,combinations_with_replacement(S,k))), sep='\n')
