b84f7763b0cd35c4b54a6a000e2a550ddfaece36
David Dobrinskiy <david@dobrinskiy.com>
2016-12-18 15:37:54 +0300

    Implement final torsional angle implementation

--- python_domain/torsional_angle/old_implementation/torsional_angle_onefile.py
+++ python_domain/torsional_angle/old_implementation/torsional_angle_onefile.py
@@ -2,34 +2,13 @@
 
 # In[6]:
 
-def vectors_angle(v1, v2):
-    # http://www.intmath.com/vectors/7-vectors-in-3d-space.php
-    # returns angle b/w vectrs in RADIANS
-    cos_theta = dot_product(v1, v2)/(v1.length*v2.length)
-    theta = acos(cos_theta)
-    return theta
-vectors_angle(AB, BC)
+
 
 
 # In[7]:
 
-def cross_product(v1, v2):
-    # https://www.mathsisfun.com/algebra/vectors-cross-product.html
-    ax, ay, az = v1.gradient
-    bx, by, bz = v2.gradient
 
-    # since we use vectorised v1, v2, we can assume they both start at the origin
-    # then AxB = (Ay*Bz - Az*By, ..., ...) [reffer to link above for full formula]
-    cx = ay*bz - az*by
-    cy = az*bx - ax*bz
-    cz = ax*by - ay*bx
 
-    # c is our resulting vector, i.e. cross-product b/w v1 and v2
-    c_tail = Point_3d( 0,  0,  0)
-    c_head = Point_3d(cx, cy, cz)
-    c = Vector(c_tail, c_head)
-
-    return c
 
 cross_product(AB, BC).gradient
 
@@ -41,8 +20,7 @@ print(cross_product(AB, BC)) # it is a vector
 
 # In[9]:
 
-M = cross_product(AB, BC)
-N = cross_product(BC, CD)
+
 
 print(M.gradient)
 print(N.gradient)
--- python_domain/torsional_angle/torsional_angle.py
+++ python_domain/torsional_angle/torsional_angle.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     c = ve.point3d("0 5 9".split())
     d = ve.point3d("1 7 2".split())
 
-    [print(obj.coords) for obj in [origin, a, b, c, d]]
+    #[print(obj.coords) for obj in [origin, a, b, c, d]]
     # a, b, c, d = [ve.point3d(*input().split()) for _ in range(4)]
 
     # INSTANTIATE INSTANCES OF WORKING VECTORS
@@ -19,17 +19,26 @@ if __name__ == "__main__":
     BC = ve.vector(b, c)
     CD = ve.vector(c, d)
 
-    [print(obj.gradient, obj.length) for obj in (AB, BC, CD)]
-    print('-'*40)
-    print('dot_product test')
+    #[print(obj.gradient, obj.length) for obj in (AB, BC, CD)]
+    #print('-'*40)
+    #print('dot_product test')
 
-    print(ve.dot_product(AB, BC))
+    #print(ve.dot_product(AB, BC))
 
-    print('-'*40)
-    print('vector angle test')
-    print(ve.vectors_angle(AB, BC))
+    #print('-'*40)
+    #print('vector angle test')
+    #print(ve.vectors_angle(AB, BC))
 
-    print('-'*40)
-    print('cross product test')
+    #print('-'*40)
+    #print('cross product test')
+    # calculate cross product vectors for plane angle calculation
+    M = ve.cross_product(AB, BC)
+    N = ve.cross_product(BC, CD)
 
-    print(ve.cross_product(AB, BC).gradient) # it is a vector
+    #print(M.gradient)
+    #print(N.gradient)
+
+    cos_phi = ve.dot_product(M, N) / (M.length*N.length)
+    phi = math.acos(cos_phi)
+    print('{0:.2f}'.format(math.degrees(phi)))
+    # 8.19
