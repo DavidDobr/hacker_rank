cc0a2e6dedb985dc8272fc98df77d5c53e87120d
David Dobrinskiy <david@dobrinskiy.com>
2016-12-25 01:01:49 +0300

    finalize reduce challenge

--- python_domain/reduce.py
+++ python_domain/reduce.py
@@ -2,12 +2,12 @@ from fractions import Fraction
 from functools import reduce
 
 def product(fracs):
-    t = # complete this line with a reduce statement
+    t = reduce(lambda x, y: x*y, fracs)
     return t.numerator, t.denominator
 
-if __name__ == '__main__':
-    fracs = []
-    for _ in range(int(input())):
-        fracs.append(Fraction(*map(int, input().split())))
-    result = product(fracs)
-    print(*result)
+input_test = ['1 2', '3 4', '10 6']
+fracs = []
+for test in input_test:
+    fracs.append(Fraction(*map(int, test.split())))
+result = product(fracs)
+print(*result)
